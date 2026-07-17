db4f57fac1345d1de6f02bf28425d0913b6125a3
Akua Amoabea <150204296+Akua-Amoabea@users.noreply.github.com>
2026-07-17 10:04:20 +0000

    Remove database table creation call from main application file

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from routes.v1 import notes
 from config.database import engine, Base
 from routes.v1.users import user_router
 
-Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
 app.include_router(user_router)
